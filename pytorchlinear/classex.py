e3a0fb28fb97af5ca1859f8a6dec523cc46617ab
lmy12367 <lfuturemy@163.com>
2025-06-18 10:33:03 +0800

    updata readme

--- pytorchlinear/classex.py
+++ pytorchlinear/classex.py
@@ -5,22 +5,21 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
-#实现定义好数据集的x值和y值
 x_data = torch.tensor([[1.0],[2.0],[3.0]])
 y_data = torch.tensor([[2.0],[4.0],[6.0]])
 
 
-#构建训练模型
+
 class LinearModel(torch.nn.Module):
     def __init__(self):
-        super(LinearModel,self).__init__()#继承父类
-        self.linear = torch.nn.Linear(1,1)#定义一个线性计算单元，继承Module块，也可以自主实现反向传播
+        super(LinearModel,self).__init__()
+        self.linear = torch.nn.Linear(1,1)
 
     def forward(self,x):
-        y_pred = self.linear(x)#可调用对象，进行了相应的forward计算
+        y_pred = self.linear(x)
         return y_pred
 
-model1 = LinearModel()  # 定义的模型结构初始化
+model1 = LinearModel()  
 model2 = LinearModel()
 model3 = LinearModel()
 model4 = LinearModel()
@@ -29,10 +28,10 @@ model5 = LinearModel()
 model7 = LinearModel()
 model8 = LinearModel()
 models = [model1,model2,model3,model4,model5,model7,model8]
-#构建损失函数的计算和优化器
+
 criterion = torch.nn.MSELoss(size_average = False)
 
-op1 = torch.optim.SGD(model1.parameters(),lr = 0.01)#以下分别尝试不同的优化器
+op1 = torch.optim.SGD(model1.parameters(),lr = 0.01)
 op2 = torch.optim.Adagrad(model2.parameters(),lr = 0.01)
 op3 = torch.optim.Adam(model3.parameters(),lr = 0.01)
 op4 = torch.optim.Adamax(model4.parameters(),lr = 0.01)
@@ -45,10 +44,9 @@ ops = [op1,op2,op3,op4,op5,op7,op8]
 titles = ['SGD','Adagrad','Adam','Adamax','ASGD','RNSprop','Rprop']
 
 fig,ax = plt.subplots(2,4,figsize = (20,10),dpi = 100)
-#ax[0][0].imshow()
-#训练过程，对每个优化器都进行尝试
+
 index = 0
-for item in zip(ops,models):#分别尝试每个优化器
+for item in zip(ops,models):
     epochs = []
     costs = []
 
@@ -66,17 +64,13 @@ for item in zip(ops,models):#分别尝试每个优化器
         loss.backward()
         op.step()
 
-    # 对训练结果进行输出
     print('w = ', model.linear.weight.item())
     print('b = ', model.linear.bias.item())
 
-    # 进行模型测试
     x_test = torch.Tensor([[4.0]])
     y_test = model(x_test)
     print('y_pred = ', y_test.data)
 
-    # 训练过程可视化
-    #plt.subplots(2,1,index)
     a = int(index /4)
     b = int(index % 4)
     ax[a][b].set_ylabel('Cost')
