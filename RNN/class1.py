8080317dffdd7defd382633444bec9bb98fd6786
lmy12367 <lfuturemy@163.com>
2025-09-04 20:33:09 +0800

    class

--- RNN/class1.py
+++ RNN/class1.py
@@ -7,16 +7,19 @@ if __name__=='__main__':
     hidden_size=2
 
     cell=torch.nn.RNNCell(input_size=input_size,hidden_size=hidden_size)
-
+    print(cell)
     dataset=torch.randn(seq_len,batch_szie,input_size)
     print(dataset)
     hidden=torch.zeros(batch_szie,hidden_size)
     print(hidden)
+
     for idx,input in enumerate(dataset):
         print('='*20,idx,'='*20)
+        print(input)
         print('input_size',input.shape)
 
         hidden=cell(input,hidden)
+        print(hidden)
 
         print('outputs size:',hidden.shape)
         print(hidden)
